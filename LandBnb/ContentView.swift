b4adc8dd4236bc029018b2cffefc847c2de5e07a
Paolo Dalex <paolodalex96@gmail.com>
2022-10-04 14:40:04 +0200

    Initial Commit

---
+++ LandBnb/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LandBnb
+//
+//  Created by Paolo D'Alessandro on 12/07/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandBnb/LandBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandBnbApp.swift
+//  LandBnb
+//
+//  Created by Paolo D'Alessandro on 12/07/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct LandBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
