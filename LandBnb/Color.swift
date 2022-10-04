90b14c7e4f2bf2dfa8666c3c7431eb6b714c8c9a
Paolo Dalex <paolodalex96@gmail.com>
2022-10-04 16:43:42 +0200

    feat: homepage UI

---
+++ LandBnb/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  LandBnb
+//
+//  Created by Paolo D'Alessandro on 12/07/1401 AP.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    public static let lightgray :Color = Color(UIColor(red:234/255, green: 234/255, blue: 234/255, alpha: 0.7))
+}
--- LandBnb/ContentView.swift
+++ LandBnb/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var search = ""
+    @State private var searchOnOver = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Ciao Jim")
-        }
-        .padding()
+        ZStack {
+            VStack {
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.lightgray)
+            .cornerRadius(20)
+            .padding([.bottom, .top], -10)
+            .padding(.horizontal, 20)
+            
+            VStack {
+                HStack {
+                    Image("logo")
+                        .padding()
+                        .frame(maxHeight: 50)
+                }
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(14)
+                
+                Spacer()
+                    .frame(maxHeight: 280)
+                Text("Dove vuoi andare?")
+                    .font(.title)
+                    .fontWeight(.bold)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("", text: $search)
+                }
+                .padding(12)
+                .background(Color.white)
+                .cornerRadius(20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(searchOnOver ? .blue.opacity(0.4) : .gray.opacity(0.4), lineWidth: 1))
+                .onTapGesture {
+                    searchOnOver.toggle()
+                }
+                Spacer()
+                TextField("", text: $search)
+
+            }
+            
+            .padding([.bottom, .top], 0)
+            .padding(.horizontal, 50)
+
+        }.background(
+            Image("surface")
+                
+        )
     }
 }
 
