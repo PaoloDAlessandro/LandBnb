b54afcf01d83d93e81914687c6933a325f219e48
Paolo Dalex <paolodalex96@gmail.com>
2022-10-31 12:13:17 +0100

    feat: added most visited countries section in home

--- LandBnb/ContentView.swift
+++ LandBnb/ContentView.swift
@@ -7,36 +7,71 @@
 
 import SwiftUI
 
+struct Country: Identifiable {
+    let name: String
+    let flag: String
+    var id: String { name }
+}
+
 struct ContentView: View {
     @State private var search = ""
     @State private var searchOnOver = false
     
+    private var countries: [Country] = [Country(name: "Spain", flag: "🇪🇸"), Country(name: "Greek", flag: "🇬🇷"), Country(name: "Norway", flag: "🇳🇴")]
+    
+    
     var body: some View {
         ZStack {
-            VStack {
-                Text("Dove vuoi andare?")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("textColor"))
-                HStack {
-                    Image(systemName: "magnifyingglass")
+            VStack{
+                Spacer()
+                    .frame(maxHeight: 300)
+                VStack {
+                    Text("Dove vuoi andare?")
+                        .font(.title)
+                        .fontWeight(.bold)
                         .foregroundColor(Color("textColor"))
-                    TextField("", text: $search)
-                        .foregroundColor(Color("textColor"))
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("textColor"))
+                        TextField("", text: $search)
+                            .foregroundColor(Color("textColor"))
+                    }
+                    .padding(12)
+                    .background(Color("textField"))
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(searchOnOver ? .blue.opacity(0.4) : .gray.opacity(0.4), lineWidth: 1))
+                    .onTapGesture {
+                        searchOnOver.toggle()
+                    }
+                    NavigationView {
+                        List {
+                            ForEach(countries) {country in
+                                NavigationLink(destination: ProductView()) {
+                                    HStack {
+                                        Text(country.flag)
+                                            .font(.title)
+                                        Text(country.name)
+                                            .font(.headline)
+                                            .fontWeight(.medium)
+                                            .foregroundColor(Color("textColor"))
+                                    }
+                                    .padding([.top, .bottom], 8)
+                                }
+                            }
+                        }
+                        .navigationBarHidden(true)
+                    }
+                    .padding(.top, 10)
                 }
-                .padding(12)
-                .background(Color("textField"))
-                .cornerRadius(20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(searchOnOver ? .blue.opacity(0.4) : .gray.opacity(0.4), lineWidth: 1))
-                .onTapGesture {
-                    searchOnOver.toggle()
-                }
+                .removeFocusOnTap()
+                .frame(maxHeight: 400)
+                .padding([.bottom, .top], 0)
+                .padding(.horizontal, 50)
+                
+                Spacer()
             }
-            .removeFocusOnTap()
-            .padding([.bottom, .top], 0)
-            .padding(.horizontal, 50)
         }
     }
 }
